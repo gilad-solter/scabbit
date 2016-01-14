b9aae736b979b8ef37bf1bec1d86b7885778a00a
GiladSolter <gilad@wefind-tech.com>
2016-01-14 22:06:52 +0200

    INITIAL SETUP SUCCESS

--- app/app.js
+++ app/app.js
@@ -3,10 +3,29 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'myApp.login',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
+
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider
+
+      .when('/login', {
+        templateUrl: 'login/login.html',
+        controller: 'LoginCtrl'
+      })
+      .when('/', {
+        redirectTo: '/login'
+      })
+      .when('/view1', {
+        templateUrl: 'view1/view1.html',
+        controller: 'View1Ctrl'
+      })
+      .when('/view2', {
+        templateUrl: 'view2/view2.html',
+        controller: 'View2Ctrl'
+      })
+      .otherwise({redirectTo: '/login'});
 }]);
---
+++ app/login/login.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Gilad on 14/1/16.
+ */
+
+'use strict';
+
+angular.module('myApp.login', [])
+
+    .controller('LoginCtrl', ['$scope', function($scope) {
+
+        $scope.name='Sachbak Olam!';
+
+
+    }]);
